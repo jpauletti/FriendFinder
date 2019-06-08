ac402d23c1b8bd9132943d1fda6e4137f92c83fa
Julia Pauletti <julia.pauletti@yahoo.com>
2019-06-08 14:03:53 -0500

    form validation cleanup & now working with post request

--- app/public/post.js
+++ app/public/post.js
@@ -1,4 +1,4 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+// bootstrap validation
 (function () {
     'use strict';
     window.addEventListener('load', function () {
@@ -10,60 +10,52 @@
                 if (form.checkValidity() === false) {
                     event.preventDefault();
                     event.stopPropagation();
+                } else {
+                    // submit survey results
+                    event.preventDefault();
+
+                    var name = $("#your-name").val().trim();
+                    var photoLink = $("#photo-link").val().trim();
+
+                    var scores = [
+                        $("#q1 input[name=questionOne]:checked").val(),
+                        $("#q2 input[name=questionTwo]:checked").val(),
+                        $("#q3 input[name=questionThree]:checked").val(),
+                        $("#q4 input[name=questionFour]:checked").val(),
+                        $("#q5 input[name=questionFive]:checked").val(),
+                        $("#q6 input[name=questionSix]:checked").val(),
+                        $("#q7 input[name=questionSeven]:checked").val(),
+                        $("#q8 input[name=questionEight]:checked").val(),
+                        $("#q9 input[name=questionNine]:checked").val(),
+                        $("#q10 input[name=questionTen]:checked").val()
+                    ];
+
+                        var newFriend = {
+                                name: name,
+                                photo: photoLink,
+                                scores: scores
+                        }
+
+                        console.log("making push request");
+
+                        // push newFriend data
+                        $.post("/api/friends", newFriend, function (data) {
+                            // receive the closestMatch and its info
+                            console.log(data);
+
+                            var matchName = data.name;
+                            var matchPhoto = data.photo;
+
+
+
+                            // open modal
+                            // show best match - name and picture
+
+                        });
+                    // }
                 }
                 form.classList.add('was-validated');
             }, false);
         });
     }, false);
 })();
-
-
-$("#submitSurvey").on("click", function (event) {
-    event.preventDefault();
-
-    var name = $("#your-name").val().trim();
-    var photoLink = $("#photo-link").val().trim();
-
-    var scores = [
-        $("#q1 input[name=questionOne]:checked").val(),
-        $("#q2 input[name=questionTwo]:checked").val(),
-        $("#q3 input[name=questionThree]:checked").val(),
-        $("#q4 input[name=questionFour]:checked").val(),
-        $("#q5 input[name=questionFive]:checked").val(),
-        $("#q6 input[name=questionSix]:checked").val(),
-        $("#q7 input[name=questionSeven]:checked").val(),
-        $("#q8 input[name=questionEight]:checked").val(),
-        $("#q9 input[name=questionNine]:checked").val(),
-        $("#q10 input[name=questionTen]:checked").val()
-    ];
-
-    var empty = false;
-    for (var i = 0; i < scores.length; i++) {
-        if (scores[i] === undefined) {
-            empty = true;
-        }
-    }
-
-    if (name !== undefined && photoLink !== undefined && !empty) {
-        var newFriend = {
-            name: name,
-            photo: photoLink,
-            scores: scores
-        }
-
-        // push newFriend data
-        $.post("/api/friends", newFriend, function (data) {
-            // receive the closestMatch and its info
-            console.log(data);
-
-            var matchName = data.name;
-            var matchPhoto = data.photo;
-
-            
-
-            // open modal
-            // show best match - name and picture
-
-        });
-    }
-})
